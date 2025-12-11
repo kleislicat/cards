c465c538e716d67c7386be8c25e3c01140d7c36e
kleislicat <79645287+kleislicat@users.noreply.github.com>
2025-12-11 18:09:06 +0000

    feat: Create cards microservice based on accounts MS

---
+++ src/main/java/com/kleislicat/cards/repository/CardsRepository.java
@@ -0,0 +1,15 @@
+package com.kleislicat.cards.repository;
+
+import com.kleislicat.cards.entity.Cards;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+  Optional<Cards> findByMobileNumber(String mobileNumber);
+
+  Optional<Cards> findByCardNumber(String cardNumber);
+
+}
